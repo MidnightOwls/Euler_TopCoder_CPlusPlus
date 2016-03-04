27d88ef234725bc63316aa809122fa126256ac97
Edward Wang <edward.wang.95@gmail.com>
2016-03-04 01:06:34 -0500

    Setup

---
+++ Euler_TopCoder_C++/Euler_TopCoder_C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Euler_TopCoder_C++
+//
+//  Created by Edward Wang on 3/4/16.
+//  Copyright © 2016 MidnightOwls. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
