7a904e70414466c9d6516c9e2558704f2d1aab8e
Edward Wang <edward.wang.95@gmail.com>
2016-03-04 01:15:51 -0500

    Working on: Euler - Gozinta Chains

---
+++ Euler_TopCoder_C++/Euler_TopCoder_C++/Euler/GozintaChains.hpp
@@ -0,0 +1,33 @@
+//
+//  GozintaChains.hpp
+//  Euler_TopCoder_C++
+//
+//  Created by Edward Wang on 3/4/16.
+//  Copyright © 2016 MidnightOwls. All rights reserved.
+//
+
+/**
+ A gozinta chain for n is a sequence {1,a,b,...,n} where each element properly divides the next.
+ There are eight gozinta chains for 12:
+ {1,12} ,{1,2,12}, {1,2,4,12}, {1,2,6,12}, {1,3,12}, {1,3,6,12}, {1,4,12} and {1,6,12}.
+ Let g(n) be the number of gozinta chains for n, so g(12)=8.
+ g(48)=48 and g(120)=132.
+ 
+ Find the sum of the numbers n not exceeding 1016 for which g(n)=n.
+ */
+
+#ifndef GozintaChains_hpp
+#define GozintaChains_hpp
+
+#include <stdio.h>
+
+namespace Euler{
+    class GozintaChains{
+    private:
+        
+    public:
+        
+    };
+}
+
+#endif /* GozintaChains_hpp */
